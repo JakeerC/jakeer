ffcea4b290379db7e8ffab49bd308bcba4128b62
JakeerC <jakeerchilakala@gmail.com>
2024-02-24 22:54:22 +0530

    feat: add theme toggle wrapper using next-themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 /* Bismillah ir rahman ir rahim */
-
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
+
+import { Providers } from '@/app/providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
