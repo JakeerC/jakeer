323e6c0f0b1b3abe39a86410be3170a1f1822c9f
JakeerC <jakeerchilakala@gmail.com>
2024-02-24 10:45:06 +0530

    initial commit - cleanup

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+/* Bismillah ir rahman ir rahim */
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Jakeer | Personal Website",
+  description: "Personal Website of Jakeer.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
