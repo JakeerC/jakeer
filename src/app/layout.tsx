a844f7eb388e078a38c239fbc4062238f0a08892
JakeerC <jakeerchilakala@gmail.com>
2024-02-24 20:01:37 +0530

    fix: eslint errors - sort imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 /* Bismillah ir rahman ir rahim */
+
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
