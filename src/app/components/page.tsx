1d0b7b8815b0cef5ff7262cea23d94abca720c61
JakeerC <jakeerchilakala@gmail.com>
2024-02-25 12:49:40 +0530

    feat(page): add components to page to view all common components available
    route: '/components'

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -2,6 +2,11 @@
 import Button from '@/components/buttons/Button';
 import ThemeButton from '@/components/buttons/ThemeButton';
 import StyledInput from '@/components/form/StyledInput';
+import ButtonLink from '@/components/links/ButtonLink';
+import CustomLink from '@/components/links/CustomLink';
+import ShareTweetButton from '@/components/links/ShareTweetButton';
+import TOCLink from '@/components/links/TOCLink';
+import UnstyledLink from '@/components/links/UnstyledLink';
 
 export default function Components() {
   return (
@@ -11,6 +16,42 @@ export default function Components() {
       <Button> Button label</Button>
 
       <StyledInput />
+
+      {/* Links */}
+
+      <UnstyledLink href={'https://github.com/JakeerC'}>
+        github account
+      </UnstyledLink>
+
+      <UnstyledLink href={'/components#unstyledlink'}>
+        UnstyledLink link
+      </UnstyledLink>
+
+      <ButtonLink href={'/components#ButtonLink'}> Button Link </ButtonLink>
+
+      <CustomLink href={'/components#customlink'}>Custom Link</CustomLink>
+
+      <ShareTweetButton
+        url="https://twitter.com/jakeerchilakala"
+        title="Jakeer twitter profile"
+      />
+
+      <TOCLink
+        text="toc link l1"
+        activeSection={'customlink'}
+        id="customlink"
+        level={1}
+        key={'customlink'}
+        minLevel={1}
+      />
+      <TOCLink
+        text="toc link l2"
+        activeSection={''}
+        id="unstyledlink"
+        level={2}
+        key={'unstyledlink'}
+        minLevel={1}
+      />
     </div>
   );
 }
