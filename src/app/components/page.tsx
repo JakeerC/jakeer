3ee33f0284077ecfb27394323ce5d2742e21bbc9
JakeerC <jakeerchilakala@gmail.com>
2024-02-25 10:47:56 +0530

    chore(comp): add styled input component

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,12 +1,16 @@
+'use client';
 import Button from '@/components/buttons/Button';
 import ThemeButton from '@/components/buttons/ThemeButton';
+import StyledInput from '@/components/form/StyledInput';
 
 export default function Components() {
   return (
-    <div className="flex flex-col mt-8 gap-4 justify-center items-center">
-      <ThemeButton> Test</ThemeButton>
+    <div className="flex flex-col mt-8 gap-4 justify-center items-center m-auto align-middle md:w-[40rem]">
+      <ThemeButton />
 
       <Button> Button label</Button>
+
+      <StyledInput />
     </div>
   );
 }
