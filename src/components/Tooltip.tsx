109f0ecdd2db8c26f6c9e9466ec9e478bb1b7192
JakeerC <jakeerchilakala@gmail.com>
2024-02-25 20:44:32 +0530

    chore(comp): add Tooltip component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,56 @@
+// !Typescript error
+// * error: Property 'children' does not exist on type 'IntrinsicAttributes & IntrinsicClassAttributes<Tooltip> & Readonly<TooltipProps>'.ts(2769)
+
+/* eslint-disable */
+import clsx from 'clsx';
+import * as React from 'react';
+import { Tooltip as TippyTooltip, TooltipProps } from 'react-tippy';
+
+type TooltipTextProps = {
+  tipChildren?: React.ReactNode;
+  children?: React.ReactNode;
+  className?: string;
+  spanClassName?: string;
+  withUnderline?: boolean;
+} & TooltipProps &
+  Omit<React.ComponentPropsWithoutRef<'div'>, 'children' | 'className'>;
+
+export default function Tooltip({
+  tipChildren,
+  children,
+  className,
+  spanClassName,
+  withUnderline = false,
+  ...rest
+}: TooltipTextProps) {
+  return (
+    <TippyTooltip
+      trigger="mouseenter"
+      interactive
+      html={
+        <div
+          className={clsx(
+            className,
+            'inline-block rounded-md bg-white p-2 text-gray-600 shadow-md dark:bg-dark dark:text-gray-200',
+            'border dark:border-gray-600 '
+          )}
+        >
+          {tipChildren}
+        </div>
+      }
+      {...rest}
+    >
+      {withUnderline ? (
+        <span
+          className={clsx(spanClassName, 'underline')}
+          style={{ textDecorationStyle: 'dotted' }}
+        >
+          {children}
+        </span>
+      ) : (
+        <>{children}</>
+      )}
+    </TippyTooltip>
+  );
+}
+/* eslint-enable */
