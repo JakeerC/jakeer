15cfc694b18191e760639cf670b662d814854709
JakeerC <jakeerchilakala@gmail.com>
2024-02-27 11:18:48 +0530

    feat: generate opengraph

---
+++ src/lib/og.ts
@@ -0,0 +1,26 @@
+import { defaultOGLogo, ogBaseURL } from '@/constants/urls';
+
+type OpenGraphType = {
+  siteName: string;
+  description: string;
+  templateTitle?: string;
+  logo?: string;
+};
+
+export function openGraph({
+  siteName,
+  templateTitle,
+  description,
+  logo = defaultOGLogo,
+}: OpenGraphType): string {
+  const ogLogo = encodeURIComponent(logo);
+  const ogSiteName = encodeURIComponent(siteName.trim());
+  const ogTemplateTitle = templateTitle
+    ? encodeURIComponent(templateTitle.trim())
+    : undefined;
+  const ogDesc = encodeURIComponent(description.trim());
+
+  return `${ogBaseURL}/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+    ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
+  }`;
+}
