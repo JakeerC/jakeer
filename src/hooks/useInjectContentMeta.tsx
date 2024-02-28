45f3d01c30737088c375368f67259dc8ded999ad
JakeerC <jakeerchilakala@gmail.com>
2024-02-28 23:45:47 +0530

    chore(utils): add useInjectContentMeta hook

---
+++ src/hooks/useInjectContentMeta.tsx
@@ -0,0 +1,57 @@
+'use client';
+// ! TODO => possible raft to RSC
+import { useQuery } from '@tanstack/react-query';
+import * as React from 'react';
+
+import { pickContentMeta } from '@/lib/contentMeta';
+import { cleanBlogPrefix } from '@/lib/helper.client';
+import { getContentMeta } from '@/lib/requests/content-meta';
+
+import { contentMetaFlag } from '@/constants/env';
+
+import {
+  ContentType,
+  InjectedMeta,
+  PickFrontmatter,
+} from '@/types/frontmatters';
+
+export default function useInjectContentMeta<T extends ContentType>(
+  type: T,
+  frontmatter: Array<PickFrontmatter<T>>
+) {
+  const { data: contentMeta, error } = useQuery({
+    queryKey: ['contents'],
+    queryFn: getContentMeta,
+    enabled: contentMetaFlag,
+  });
+  const isLoading = !error && !contentMeta;
+  const meta = React.useMemo(
+    () => pickContentMeta(contentMeta, type),
+    [contentMeta, type]
+  );
+
+  type PopulatedContent = Array<PickFrontmatter<T> & InjectedMeta>;
+
+  const [populatedContent, setPopulatedContent] =
+    React.useState<PopulatedContent>(
+      () => [...frontmatter] as PopulatedContent
+    );
+
+  React.useEffect(() => {
+    if (meta) {
+      const mapped = frontmatter.map(fm => {
+        const views = meta.find(
+          meta => meta.slug.replace('|', '/') === cleanBlogPrefix(fm.slug)
+        )?.views;
+        const likes = meta.find(
+          meta => meta.slug.replace('|', '/') === cleanBlogPrefix(fm.slug)
+        )?.likes;
+        return { ...fm, views, likes };
+      });
+
+      setPopulatedContent(mapped);
+    }
+  }, [meta, isLoading, frontmatter]);
+
+  return populatedContent;
+}
