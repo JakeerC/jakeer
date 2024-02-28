b824c07f3c7773cf99cc8837358b674f51331236
JakeerC <jakeerchilakala@gmail.com>
2024-02-28 11:22:31 +0530

    feat(comp): add Header component

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,104 @@
+'use client';
+import clsx from 'clsx';
+import { usePathname, useRouter } from 'next/navigation';
+import * as React from 'react';
+
+import Accent from '@/components/Accent';
+import ThemeButton from '@/components/buttons/ThemeButton';
+import UnstyledLink from '@/components/links/UnstyledLink';
+import { navLinks } from '@/constants/consts';
+
+type HeaderProps = {
+  large?: boolean;
+};
+
+export default function Header({ large = false }: HeaderProps) {
+  //#region  //*=========== Route Functionality ===========
+  const router = useRouter();
+  const pathname = usePathname();
+  console.log(router);
+  console.log(pathname);
+
+  /** Ex: /projects/petrolida-2021 -> ['', 'projects', 'petrolida-2021'] */
+  const arrOfRoute = pathname.split('/');
+  const baseRoute = '/' + arrOfRoute[1];
+  //#endregion  //*======== Route Functionality ===========
+
+  //#region  //*=========== Scroll Shadow ===========
+  const [onTop, setOnTop] = React.useState<boolean>(true);
+  React.useEffect(() => {
+    const handleScroll = () => {
+      setOnTop(window.pageYOffset === 0);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  //#endregion  //*======== Scroll Shadow ===========
+
+  return (
+    <header
+      className={clsx(
+        'sticky top-0 z-50 transition-shadow',
+        !onTop && 'shadow-sm'
+      )}
+    >
+      {/* Skip Navigation */}
+      <a
+        href="#skip-nav"
+        className={clsx(
+          'rounded-sm p-2 transition',
+          'font-medium text-black dark:text-white',
+          'bg-white dark:bg-dark',
+          'group dark:hover:text-primary-300',
+          'focus:outline-none focus:ring focus:ring-primary-300',
+          'absolute left-4 top-4',
+          '-translate-y-16 focus:translate-y-0'
+        )}
+      >
+        <Accent>Skip to main content</Accent>
+      </a>
+
+      {/* Gradient List */}
+      <div className="h-2 bg-gradient-to-tr from-primary-200 via-primary-300 to-primary-400" />
+
+      <div className="bg-white transition-colors dark:bg-dark dark:text-white">
+        <nav
+          className={clsx(
+            'layout flex items-center justify-between py-4',
+            large && 'lg:max-w-[68rem]'
+          )}
+        >
+          <ul className="flex items-center justify-between space-x-3 text-xs md:space-x-4 md:text-base">
+            {navLinks.map(({ href, label }) => (
+              <li key={`${href}${label}`}>
+                <UnstyledLink
+                  href={href}
+                  className={clsx(
+                    'rounded-sm py-2 transition-colors',
+                    'font-medium text-black dark:text-white',
+                    'group dark:hover:text-primary-300',
+                    'focus:outline-none focus-visible:ring focus-visible:ring-primary-300'
+                  )}
+                >
+                  <span
+                    className={clsx(
+                      'transition-colors',
+                      'bg-primary-300/0 group-hover:bg-primary-300/20 dark:group-hover:bg-primary-300/0',
+                      href === baseRoute &&
+                        '!bg-primary-300/50 dark:bg-gradient-to-tr dark:from-primary-300 dark:to-primary-400 dark:bg-clip-text dark:text-transparent'
+                    )}
+                  >
+                    {label}
+                  </span>
+                </UnstyledLink>
+              </li>
+            ))}
+          </ul>
+          <ThemeButton />
+        </nav>
+      </div>
+    </header>
+  );
+}
