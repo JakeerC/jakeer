080439e633fb11147438615b28266dce4017fe9e
JakeerC <jakeerchilakala@gmail.com>
2024-03-06 16:30:33 +0530

    build(pkg): implement  nextjs-toploader for  page load feedback on top of screen

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 const queryClient = new QueryClient();
+import NextTopLoader from 'nextjs-toploader';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
@@ -10,6 +11,11 @@ export function Providers({ children }: { children: React.ReactNode }) {
       defaultTheme="dark"
       enableSystem={false}
     >
+      <NextTopLoader
+        color="linear-gradient(90deg, rgba(219,255,0,1) 0%, rgba(0,232,135,1) 35%, rgba(0,224,243,1) 100%)"
+        height={4}
+        showSpinner={false}
+      />
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </NextThemesProvider>
   );
