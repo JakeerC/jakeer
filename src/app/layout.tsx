2f777accb490c87504ca97233e337c90bed41ddc
JakeerC <jakeerchilakala@gmail.com>
2024-03-08 09:25:53 +0530

    feat(seo): add overall metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,26 @@ import Footer from '@/components/layout/Footer';
 import Header from '@/components/layout/Header';
 
 import { Providers } from '@/app/providers';
+import { domain } from '@/constants/urls';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Jakeer | Personal Website',
   description: 'Personal Website of Jakeer.',
+  applicationName: 'Jakeer Personal Site',
+  authors: [{ name: 'JakeerC', url: domain }],
+  creator: 'Jakeer',
+  generator: 'Next.js',
+  referrer: 'origin-when-cross-origin',
+  keywords: ['Next.js', 'React', 'JavaScript'],
+  metadataBase: new URL('/', domain),
+  themeColor: [
+    { media: '(prefers-color-scheme: dark)', color: '#0e1111' },
+    { media: '(prefers-color-scheme: light)', color: '#ffffff' },
+  ],
+  robots: 'follow, index',
+  openGraph: { images: ['/favicon/large-og.jpeg'] },
 };
 
 export default function RootLayout({
