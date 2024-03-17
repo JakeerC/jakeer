014814bbcb2b9f007029824a92428498caa3ff91
JakeerC <jakeerchilakala@gmail.com>
2024-03-17 12:33:07 +0530

    fix: template title

--- src/lib/og.ts
+++ src/lib/og.ts
@@ -36,8 +36,8 @@ type OpenGraphType =
  * @param siteName - The name of the website or page.
  * @param description - The description of the website or page.
  * @param logo - The URL of the logo to use for the Open Graph meta tag. /the URL of the logo to use (optional, default is the website's logo)
- * @param logoWidth - The width of the logo. @default 250
- * @param logoHeight - The height of the logo. @default 250
+ * @param logoWidth - The width of the logo. @default 480
+ * @param logoHeight - The height of the logo. @default 480
  * @param ogType - The type of Open Graph object to generate. for example "gradient" or "general" or "article"
  * @param articleType - the type of article (e.g. "projects", "blog", or "snippets")
  * @param banner - The banner image to use for the Open Graph object.
@@ -48,23 +48,23 @@ type OpenGraphType =
 export function og({
   siteName = 'Jakeer Website',
   description = 'Site description',
-  templateTitle = 'Default template title for the Open Graph',
+  templateTitle = '',
   logo = defaultOGLogo,
   banner = '',
-  logoWidth = 250,
-  logoHeight = 250,
+  logoWidth = 480,
+  logoHeight = 480,
   theme = 'dark',
   ogType = 'general',
   articleType,
 }: OpenGraphType): string {
   const encodedLogo = encodeURIComponent(logo);
   const encodedSiteName = encodeURIComponent(siteName.trim());
-  const encodedTemplateTitle = encodeURIComponent(templateTitle.trim());
+  // const encodedTemplateTitle = encodeURIComponent(templateTitle.trim());
 
   const encodedDesc = encodeURIComponent(description.trim());
   const encodedBanner = encodeURIComponent(banner.trim());
   if (ogType === 'general' || ogType === 'gradient') {
     return `${ogBaseURL}/api/${ogType}?siteName=${encodedSiteName}&description=${encodedDesc}&logo=${encodedLogo}&Width=${logoWidth}&Height=${logoHeight}&theme=${theme}`;
   }
-  return `${ogBaseURL}/api/${ogType}?articleType=${articleType}&banner=${encodedBanner}&templateTitle=${encodedTemplateTitle}&theme=${theme}`;
+  return `${ogBaseURL}/api/${ogType}?articleType=${articleType}&banner=${encodedBanner}&templateTitle=${templateTitle}&theme=${theme}`;
 }
