f4417b21443f2d7c1633e871f4d7602c037e30a5
JakeerC <jakeerchilakala@gmail.com>
2024-04-21 19:32:59 +0530

    refactor: constants

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -13,4 +13,14 @@ export const defaultOGLogo = `${ogBaseURL}/images/logo.png`;
 //#endregion  //*======== open graph setup ===========
 
 export const subStackURL = 'https://jakeer.substack.com/';
+
 export const domain = 'https://jakeer.vercel.app';
+
+export const twitterURL = 'https://twitter.com/jakeerchilakala';
+
+export const linkedInURL = 'https://www.linkedin.com/in/chilakalajakeer';
+
+export const githubURL = 'https://github.com/jakeerc';
+
+export const personalProfilePicture =
+  'https://res.cloudinary.com/jakeer-cloudinary/image/upload/q_auto,f_auto,w_2560/personal/logo_round';
