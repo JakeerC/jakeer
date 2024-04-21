15c13a75387c1750ae93e68b1e5e92670b31ff6a
JakeerC <jakeerchilakala@gmail.com>
2024-04-21 19:57:59 +0530

    refactor: remove border

--- src/components/buttons/ThemeButton.tsx
+++ src/components/buttons/ThemeButton.tsx
@@ -16,16 +16,15 @@ export default function ThemeButton({ className, ...rest }: ThemeButtonProps) {
     <button
       className={clsx(
         'rounded-md p-2 focus:outline-none md:p-2.5',
-        'border dark:border-gray-600',
         'hover:border-primary-300 hover:text-primary-300 dark:hover:border-primary-300 dark:hover:text-primary-300',
-        'focus-visible:border-primary-300 focus-visible:text-primary-300 dark:focus-visible:border-primary-300 dark:focus-visible:text-primary-300',
+        'focus:border focus-visible:border-primary-300 focus-visible:text-primary-300 dark:focus-visible:border-primary-300 dark:focus-visible:text-primary-300',
         'text-lg md:text-xl',
         className
       )}
       {...rest}
       onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
-      {mounted ? <>{theme === 'light' ? <FiMoon /> : <FiSun />}</> : <FiSun />}
+      {mounted ? <>{theme === 'dark' ? <FiMoon /> : <FiSun />}</> : <FiSun />}
     </button>
   );
 }
