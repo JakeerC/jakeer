c1f671286e586d7e9be0c19d50f87b469b6498c7
JakeerC <jakeerchilakala@gmail.com>
2024-04-22 22:33:18 +0530

    fix: image cover container instead of stretch on small devices

--- src/app/blog/[slug]/Post.tsx
+++ src/app/blog/[slug]/Post.tsx
@@ -111,16 +111,14 @@ export default function Post({
             width={1200}
             height={(1200 * 2) / 5}
             aspect={{ height: 2, width: 5 }}
-            imgClassName="h-full w-full"
+            imgClassName="object-cover w-full h-full"
             className={clsx(
               'overflow-hidden -z-1',
               'max-w-[calc(100vw_+_calc(100vw_-_100%))]',
               'absolute top-0 -left-[calc(100vw_-_100%)] -right-[calc(100vw_-_100%)]',
               'pointer-events-none blur transition',
               'saturate-125 opacity-40 dark:opacity-65',
-              // eslint-disable-next-line max-len
               '[mask-image:linear-gradient(to_bottom,rgba(0,0,0,0.9)_0%,rgba(0,0,0,0)_100%)]'
-              // 'h-[85vh] max-h-[384] w-100vw'
             )}
             style={{ height: '85vh', maxHeight: 384, width: '100vw' }}
           />
