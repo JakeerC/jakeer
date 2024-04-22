5d657f2df81fe5e064d4bc4197b562cb6b0579d5
JakeerC <jakeerchilakala@gmail.com>
2024-04-22 12:09:30 +0530

    chore: add util to convert color from het to rgb

---
+++ src/lib/color.ts
@@ -0,0 +1,18 @@
+import { colord } from 'colord';
+
+export const hexToRgb = (
+  hex?: string | null,
+  alpha?: number,
+  numbersOnly?: boolean
+): string | null => {
+  if (!hex) return null;
+  let color = colord(hex);
+  if (!numbersOnly) color = color.alpha(alpha === 0 ? 0 : alpha || 1);
+  const { r, g, b, a } = color.toRgb();
+  if (numbersOnly) return `${r} ${g} ${b}`;
+  if (alpha) {
+    return `rgba(${r} ${g} ${b} / ${a})`;
+  } else {
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+};
