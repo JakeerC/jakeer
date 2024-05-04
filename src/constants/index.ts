30026a114ef34b31a9b3a62c2f637609bf039b75
JakeerC <jakeerchilakala@gmail.com>
2024-05-04 10:59:56 +0530

    refactor:  remove  `ARTICLE_MAX_WIDTH`

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -21,11 +21,7 @@ export {
 } from './seoMetaData';
 export { socialLinks } from './socialLinks';
 export { techList } from './techList';
-export {
-  ARTICLE_MAX_WIDTH,
-  THEME_COLOR_DARK,
-  THEME_COLOR_LIGHT,
-} from './theme';
+export { THEME_COLOR_DARK, THEME_COLOR_LIGHT } from './theme';
 export {
   defaultOGLogo,
   domain,
